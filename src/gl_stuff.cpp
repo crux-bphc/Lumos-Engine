cf3914b32c3e5750e7eb35c210a1fbb91403da35
i-ideamama <advaith.n@gmail.com>
2024-02-21 18:23:07 +0530

    Added structure of doxygen docs

--- src/gl_stuff.cpp
+++ src/gl_stuff.cpp
@@ -2,29 +2,59 @@
 
 
 
-
+/**
+ * @brief Construct a new VAO::VAO object
+ * 
+ */
 VAO::VAO() {
     glGenVertexArrays(1, &ref);
 }
 
+/**
+ * @brief Destroy the VAO::VAO object
+ * 
+ */
 VAO::~VAO() {
     glDeleteVertexArrays(1, &ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void VAO::bind() {
     glBindVertexArray(ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void VAO::unbind() {
     glBindVertexArray(0);
 } 
 
+/**
+ * @brief 
+ * 
+ * @param count 
+ */
 void VAO::draw(int count) {
     this->bind();
     glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, (void *)0);
     this->unbind();
 }
 
+/**
+ * @brief 
+ * 
+ * @param vbo 
+ * @param ref 
+ * @param numVals 
+ * @param type 
+ * @param stride 
+ * @param offset 
+ */
 void VAO::linkEnableAttrib(VBO* vbo, int ref, int numVals, GLenum type, int stride, void* offset) {
     this->bind();
     vbo->bind();
@@ -34,6 +64,11 @@ void VAO::linkEnableAttrib(VBO* vbo, int ref, int numVals, GLenum type, int stri
     this->unbind();
 }
 
+/**
+ * @brief 
+ * 
+ * @param ebo 
+ */
 void VAO::attachIndex(EBO* ebo) {
     this->bind();
     ebo->bind();
@@ -41,60 +76,114 @@ void VAO::attachIndex(EBO* ebo) {
     ebo->unbind();
 }
 
-
-
+/**
+ * @brief Construct a new VBO::VBO object
+ * 
+ */
 VBO::VBO() {
     glGenBuffers(1, &ref);
 }
 
+/**
+ * @brief Destroy the VBO::VBO object
+ * 
+ */
 VBO::~VBO() {
     glDeleteBuffers(1, &ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void VBO::bind() {
     glBindBuffer(GL_ARRAY_BUFFER, ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void VBO::unbind() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
+/**
+ * @brief 
+ * 
+ * @param vertices 
+ * @param size 
+ */
 void VBO::addData(GLfloat *vertices, int size) {
     this->bind();
     glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)size, vertices, GL_DYNAMIC_DRAW);
     this->unbind();
 }
 
+/**
+ * @brief 
+ * 
+ * @param vertices 
+ * @param size 
+ * @param offset 
+ */
 void VBO::updateData(GLfloat *vertices, int size, int offset) {
     this->bind();
     glBufferSubData(GL_ARRAY_BUFFER, offset, size, vertices);
     this->unbind();
 }
 
-
-
+/**
+ * @brief Construct a new EBO::EBO object
+ * 
+ */
 EBO::EBO() {
     glGenBuffers(1, &ref);
 };
 
+/**
+ * @brief Destroy the EBO::EBO object
+ * 
+ */
 EBO::~EBO() {
     glDeleteBuffers(1, &ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void EBO::bind() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ref);
 }
 
+/**
+ * @brief 
+ * 
+ */
 void EBO::unbind() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
+/**
+ * @brief 
+ * 
+ * @param indices 
+ * @param size 
+ */
 void EBO::addData(GLuint *indices, int size) {
     this->bind();
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_DYNAMIC_DRAW);
     this->unbind();
 }
 
+/**
+ * @brief 
+ * 
+ * @param indices 
+ * @param size 
+ * @param offset 
+ */
 void EBO::updateData(GLuint *indices, int size, int offset) {
     this->bind();
     glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, size, indices);
@@ -102,7 +191,10 @@ void EBO::updateData(GLuint *indices, int size, int offset) {
 }
 
 
-
+/**
+ * @brief Construct a new renderer::renderer object
+ * 
+ */
 renderer::renderer() { 
     shader = new Shader("./assets/shaders/vert.glsl", "./assets/shaders/frag.glsl");
     vao.attachIndex(&ebo);
@@ -121,6 +213,13 @@ renderer::renderer() {
     
 }
 
+/**
+ * @brief 
+ * 
+ * @param index 
+ * @param vData 
+ * @param iData 
+ */
 void renderer::updateData(int index, vertTexQuad* vData, indexData iData) {
     if (vData != nullptr) {
         vbo.updateData((float*)vData, sizeof(vertTexQuad), index*sizeof(vertTexQuad));
@@ -129,15 +228,21 @@ void renderer::updateData(int index, vertTexQuad* vData, indexData iData) {
     ebo.updateData(iData.indexData, sizeof(indexData), index*sizeof(indexData));
 }
 
+/**
+ * @brief 
+ * 
+ */
 void renderer::draw() {
     shader->Activate();
     vao.draw(vbo_pos * 6);
 }
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int renderer::getFree() {
     vbo_pos = vbo_pos + 1;
     return vbo_pos - 1;  
 }
-
-
-
-
